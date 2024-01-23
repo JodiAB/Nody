abe079893282573ee9ab9ca2510b6749cb9b6601
JodiAB <jodiabrahams9@gmail.com>
2024-01-23 16:28:46 +0200

    first commit

---
+++ nody/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="fire">
+    <nav> <Navi /></nav>
+   
+  
+    <router-view />
+    <Spinner />
+    <Footer />
+  </div>
+</template>
+<script>
+import Navi from '@/components/Navi.vue'; // Adjust the import path based on your project structure
+import Footer from '@/components/Footer.vue';
+import Spinner from '@/components/Spinner.vue';
+export default {
+  components: {
+    Navi,
+    Footer,
+    Spinner
+
+    
+  
+  },
+};
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: beige;
+  background-color: navy;
+  height: 100vh;
+}
+
+nav {
+  padding: 13px;
+  display: flex;
+  justify-content: center;
+  gap:1rem;
+  color: beige;
+}
+
+nav a {
+  font-weight: bold;
+  color: beige;
+
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ nody/src/components/Navi.vue
@@ -0,0 +1,37 @@
+<!-- NavBar.vue -->
+
+<template>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/shop">Shop</router-link> |
+      <router-link to="/Cart">Cart</router-link> |
+      <router-link to="/Contact">Contact</router-link> |
+      <router-link to="/Admin">Admin</router-link>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+
+  };
+  </script>
+  
+  <style scoped>
+  nav {
+    
+    padding: 10px;
+    background-color: transparent;
+    text-align: right;
+  }
+  
+  nav a {
+    font-weight: bold;
+    color: darkgoldenrod;
+  }
+  
+  nav a.router-link-exact-active {
+    color: beige;
+  }
+  </style>
+  
